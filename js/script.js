d8505597a786da7eb7aefdfb20e0f9922036f908
Hajar CHAKIR <chakir.hajar05@gmail.com>
2025-06-30 11:44:08 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+  alert("Bienvenue dans le portfolio de Hajar Chakir !");
+});
+
+<script>
+  window.addEventListener("load", () => {
+    document.getElementById("loader").style.display = "none";
+  });
+</script>
+
